bb7fad497b558a9a39c87f9bea7f807f0d978ab4
igaki <github@musictale.net>
2021-07-12 10:21:04 +0900

    AppTest失敗

--- app/src/test/java/sequality/AppTest.java
+++ app/src/test/java/sequality/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
   @Test
   public void testAppHasAGreeting() {
     App classUnderTest = new App();
-    assertEquals("Hello World!", classUnderTest.getGreeting());
+    assertEquals("Hello World", classUnderTest.getGreeting());
 
   }
 }
